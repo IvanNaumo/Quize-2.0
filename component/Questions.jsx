048163d930ca14f4c3ed34a7348e3b9a7330bb69
IvanNaumo <vanya.naumov.95@mail.ru>
2023-12-08 12:48:13 +0300

    Router

---
+++ component/Questions.jsx
@@ -0,0 +1,44 @@
+const React = require("react");
+const Layout = require("../Layout");
+
+function Questions({ question, index, user }) {
+    return (
+      <Layout user={user}>
+        <div
+          className="card text-bg-dark mb-3"
+          data-id={question.themeId}
+          style={{ width: "50rem" }}
+        >
+          <img src={question.img} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{question.name}</h5>
+          </div>
+        </div>
+        <form action="" method="POST" className="form-answer" data-id={question.id}>
+          <div className="mb-3 parent">
+            <label htmlFor="exampleInputPassword1" className="form-label" style={{ color: "#ffc107" }}>
+              Введите ответ
+            </label>
+            <input
+              type="text"
+              className="form-control"
+              id="exampleInputPassword1"
+              name="answer"
+            />
+            <button type="submit" className="btn buttonAuth btn-outline-warning">
+              Ответить
+            </button>
+          </div>
+        </form>
+        <div className="answer"></div>
+        <a
+          href={`/questions/${index}/themes/${question.themeId}`}
+          className="btn btn-next btn-outline-warning"
+        >
+          Следующий вопрос
+        </a>
+      </Layout>
+    );
+  }
+
+module.exports = Questions;
